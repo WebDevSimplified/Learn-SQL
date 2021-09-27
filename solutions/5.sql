ea36517da0fe7a6446d1e40e8ca7a5e8d724c610
Lzy-slh <51170442+lzyslh@users.noreply.github.com>
2021-09-27 02:58:54 +0100

    Updating a query that didn't work.

--- solutions/5.sql
+++ solutions/5.sql
@@ -1,5 +1,15 @@
+/* MY SUGGESTION */
 SELECT bands.name AS 'Band Name'
+FROM bands 
+LEFT JOIN albums ON bands.id = albums.band_id
+WHERE albums.band_id IS NULL
+
+
+
+/* ORIGINAL QUERIES */
+/* PS: it didn't work for me :\ */
+/*SELECT bands.name AS 'Band Name'
 FROM bands
 LEFT JOIN albums ON bands.id = albums.band_id
 GROUP BY albums.band_id
-HAVING COUNT(albums.id) = 0;
+HAVING COUNT(albums.id) = 0;*/
