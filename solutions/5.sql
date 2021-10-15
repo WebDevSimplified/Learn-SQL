9ed5a74fac430cad78c7ed8993f443b319333a6a
WideTracked <30569190+WideTracked@users.noreply.github.com>
2021-10-15 23:43:58 +0800

    Update 5.sql

--- solutions/5.sql
+++ solutions/5.sql
@@ -3,3 +3,12 @@ FROM bands
 LEFT JOIN albums ON bands.id = albums.band_id
 GROUP BY albums.band_id
 HAVING COUNT(albums.id) = 0;
+
+/* 
+Also a valid solution:
+
+SELECT bands.name AS 'Band Name'
+FROM bands
+LEFT JOIN albums ON bands.id = albums.band_id
+WHERE albums.id IS NULL;
+*/
