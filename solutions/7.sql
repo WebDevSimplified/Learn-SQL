d6b99cc0d171fb1581b9ad643f7c8c62bf063671
Sai-Nandan-Desetti <80631906+Sai-Nandan-Desetti@users.noreply.github.com>
2023-05-26 22:29:40 +0530

    Update 7.sql
    No hardcoding the ids.

--- solutions/7.sql
+++ solutions/7.sql
@@ -3,6 +3,18 @@
   SELECT * FROM albums where release_year IS NULL;
 */
 
+/*
 UPDATE albums
 SET release_year = 1986
 WHERE id = 4;
+*/
+
+/* 
+The above solution doesn't seem practical
+if you have many different ids whose release_year IS NULL
+*/
+
+UPDATE albums,
+	(SELECT id FROM albums WHERE release_year IS NULL) AS not_yet_released
+SET albums.release_year = 1986
+WHERE albums.id = not_yet_released.id;
