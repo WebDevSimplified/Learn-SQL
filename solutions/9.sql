96c4b64af0a11d15787de908bb17afb59515feaa
Sai-Nandan-Desetti <80631906+Sai-Nandan-Desetti@users.noreply.github.com>
2023-05-26 23:49:23 +0530

    Update 9.sql

--- solutions/9.sql
+++ solutions/9.sql
@@ -4,6 +4,7 @@
   ORDER BY id DESC LIMIT 1;
 */
 
+/*
 DELETE FROM albums
 WHERE id = 19;
 
@@ -15,3 +16,21 @@ WHERE id = 19;
 
 DELETE FROM bands
 WHERE id = 8;
+*/
+
+/*
+No harcoding the id
+*/
+DELETE FROM albums
+WHERE albums.band_id
+IN (SELECT id FROM bands WHERE name = 'SSSPSN');
+
+SELECT * FROM albums;
+
+/*
+Note the apparently redundant but necessary use of the SELECT * subquery
+*/
+DELETE FROM bands
+WHERE id IN (SELECT * FROM (SELECT id FROM bands WHERE name = 'SSSPSN') AS to_delete);
+
+SELECT * FROM bands;
