530f71b6f49a2fb2e213750641390e04f6a8262a
Sai-Nandan-Desetti <80631906+Sai-Nandan-Desetti@users.noreply.github.com>
2023-05-27 00:28:58 +0530

    Update 11.sql

--- solutions/11.sql
+++ solutions/11.sql
@@ -1,3 +1,4 @@
+/*
 SELECT
   albums.name AS 'Album',
   albums.release_year AS 'Release Year',
@@ -5,3 +6,12 @@ SELECT
 FROM albums
 JOIN songs ON albums.id = songs.album_id
 GROUP BY songs.album_id;
+*/
+
+SELECT albums.name AS 'Name', albums.release_year AS 'Release Year', longest_songs.duration AS 'Duration'
+FROM albums
+INNER JOIN (
+	SELECT album_id, max(length) AS duration FROM songs
+	GROUP BY album_id
+) AS longest_songs
+ON albums.id = longest_songs.album_id;
