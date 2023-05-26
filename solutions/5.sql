ca24e4419a5a9604b406da6b49c9a2096fa844e8
Sai-Nandan-Desetti <80631906+Sai-Nandan-Desetti@users.noreply.github.com>
2023-05-26 22:40:44 +0530

    Update 5.sql
    Using non-aggregate columns in SELECT with the default modes in MySQL Workbench.

--- solutions/5.sql
+++ solutions/5.sql
@@ -1,5 +1,21 @@
+/*
 SELECT bands.name AS 'Band Name'
 FROM bands
 LEFT JOIN albums ON bands.id = albums.band_id
 GROUP BY albums.band_id
 HAVING COUNT(albums.id) = 0;
+*/
+
+/*
+The given solution throws an error:
+Error Code: 1055. Expression #1 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'record_company.bands.name' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by
+*/
+SELECT bands.name AS 'Band Name'
+FROM bands 
+INNER JOIN (
+	SELECT bands.id as id
+	FROM bands
+	LEFT JOIN albums ON bands.id = albums.band_id
+	WHERE albums.band_id IS NULL
+) AS bands_with_no_albums
+ON bands.id = bands_with_no_albums.id;
